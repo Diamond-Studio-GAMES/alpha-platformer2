ef7e4210e7d47cd3a8086495522062e42a2b24f7
Summersay415 <summersay415@gmail.com>
2023-07-09 10:14:13 +0700

    Reworked block!!!!!

--- scripts/game/other/block.gd
+++ scripts/game/other/block.gd
@@ -2,25 +2,30 @@ extends KinematicBody2D
 class_name Pushable, "res://textures/blocks/ladder.png"
 
 
+export (float) var push_speed = 50
 var _move = Vector2()
 var MAX_GRAVITY = 250
 var GRAVITY_SPEED = 750
-onready var ray0 = $ray0
-onready var ray1 = $ray1
+var player : Player
 
 
 func _physics_process(delta):
 	if _move.x == 0:
 		_move = Vector2(0, min(_move.y + GRAVITY_SPEED * delta, MAX_GRAVITY))
-	move_and_slide(_move)
+	_move = move_and_slide(_move)
 	_move.x = 0
-	if ray0.is_colliding():
-		if ray0.get_collider() is Entity:
-			var body = ray0.get_collider()
-			_move.x = max(body._move.x, 0)
-			_move.y = max(body._move.y, 0)
-	if ray1.is_colliding():
-		if ray1.get_collider() is Entity:
-			var body = ray1.get_collider()
-			_move.x = min(body._move.x, 0)
-			_move.y = max(body._move.y, 0)
+	if player:
+		for i in player.get_slide_count():
+			var col = player.get_slide_collision(i)
+			if col.collider == self:
+				_move.x += col.remainder.x * push_speed
+
+
+func body_entered(body):
+	if body is Player:
+		player = body
+
+
+func body_exited(body):
+	if body is Player:
+		player = null
