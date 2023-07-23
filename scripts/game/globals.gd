fc78ce3d5f7837c5fcc5896aab9a8b222c4455f1
Summersay415 <summersay415@gmail.com>
2023-07-23 13:15:23 +0700

    Bump version to 0.8.0 beta 1

--- scripts/game/globals.gd
+++ scripts/game/globals.gd
@@ -3,9 +3,9 @@ class_name Globals, "res://textures/gui/alpha_text.png"
 
 
 const VERSION = "0.8.0"
-const VERSION_STATUS = "сборка"
-const VERSION_STATUS_NUMBER = "5"
-const VERSION_CODE = 59
+const VERSION_STATUS = "бета"
+const VERSION_STATUS_NUMBER = "1"
+const VERSION_CODE = 60
 
 var main_file: ConfigFile
 var save_file: ConfigFile
