e7ceed1694d2520b0ce5277b7d5e93271e8acc38
Summersay415 <summersay415@gmail.com>
2023-08-28 21:46:51 +0700

    Bump version to build 2

--- scripts/game/globals.gd
+++ scripts/game/globals.gd
@@ -4,8 +4,8 @@ class_name Globals, "res://textures/gui/alpha_text.png"
 
 const VERSION = "0.9.0"
 const VERSION_STATUS = "build"
-const VERSION_STATUS_NUMBER = "1"
-const VERSION_CODE = 66
+const VERSION_STATUS_NUMBER = "2"
+const VERSION_CODE = 67
 
 var main_file: ConfigFile
 var save_file: ConfigFile
