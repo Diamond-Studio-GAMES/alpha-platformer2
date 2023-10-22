2056a1defc7722c360c0c17950100a6710a4b058
Summersay415 <summersay415@gmail.com>
2023-10-22 11:17:24 +0700

    Some improvements

--- scripts/game/player/player.gd
+++ scripts/game/player/player.gd
@@ -531,6 +531,7 @@ func _physics_process(delta):
 		_coyote_timer -= delta
 	
 	#HEALTH
+	if MP.auth(self):
 		if Input.is_action_just_pressed("potion1"):
 			use_potion(1)
 		if Input.is_action_just_pressed("potion2"):
