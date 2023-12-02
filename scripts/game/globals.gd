d4022c6f1b2e6eac56c82869c0a90d9614744e7b
Summersay415 <summersay415@gmail.com>
2023-12-02 20:53:37 +0700

    Bump code, fix some dependencies

--- scripts/game/globals.gd
+++ scripts/game/globals.gd
@@ -5,7 +5,7 @@ class_name Globals, "res://textures/gui/alpha_text.png"
 const VERSION = "1.0"
 const VERSION_STATUS = "build"
 const VERSION_STATUS_NUMBER = "1"
-const VERSION_CODE = 75
+const VERSION_CODE = 76
 
 var main_file: ConfigFile
 var save_file: ConfigFile
