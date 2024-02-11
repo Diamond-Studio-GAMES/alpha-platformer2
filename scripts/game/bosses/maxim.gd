58de6f5e9a86078159a4e5f0316751214aeeef13
Summersay415 <summersay415@gmail.com>
2024-02-11 16:55:43 +0700

    Add Boss attacks

--- scripts/game/bosses/maxim.gd
+++ scripts/game/bosses/maxim.gd
@@ -2,9 +2,9 @@ extends Boss
 
 
 var phases_attacks = {
-	1 : ["laser", "bullets"],
-	2 : ["laser", "bullets", "super_floor", "tnt"],
-	3 : ["laser", "bullets", "super_floor", "tnt"]
+	1 : ["bullets", "super_floor", "melee"],
+	2 : ["laser", "bullets", "super_floor", "tnt", "melee"],
+	3 : ["laser", "bullets", "super_floor", "tnt", "melee"]
 }
 var defeat_phrases = [
 	tr("9_10.boss.death.0"),
@@ -14,16 +14,36 @@ var defeat_phrases = [
 ]
 var face_defeated = load("res://textures/bosses/maxim/head_defeat.tres")
 var face_mercy = load("res://textures/bosses/maxim/head_mercy.tres")
+var greenball = load("res://prefabs/bosses/greenball.tscn")
+var time_fast = load("res://prefabs/bosses/time_fast.tscn")
+var blue_tnt = load("res://prefabs/bosses/blue_tnt.tscn")
+
 onready var break_on_death = {
 	$visual/body/controller : load("res://textures/bosses/maxim/controller_broken.tres"),
 	$visual/body/arm_right/hand : load("res://textures/bosses/maxim/weapon_arm_broken.tres"),
 	$visual/body/leg_left/foot : load("res://textures/bosses/maxim/weapon_leg_broken.tres"),
 }
 var is_time_faster = false
+var time_fasting
+var animation
+var track_idx
+var key_idx0
+var key_idx1
 onready var timer = $timer
+onready var floor_attack = $"../floor_attack/anim"
+onready var shoot = $visual/body/arm_right/hand/shoot
+onready var ball_spawns = [
+	$visual/body/arm_right/hand/ball_spawn2,
+	$visual/body/arm_right/hand/ball_spawn3,
+	$visual/body/arm_right/hand/ball_spawn4,
+]
 
 
 func _ready():
+	animation = anim.get_animation("bullets")
+	track_idx = animation.find_track(@"visual/body/arm_right:rotation_degrees")
+	key_idx0 = animation.track_find_key(track_idx, 0.4)
+	key_idx1 = animation.track_find_key(track_idx, 1)
 	mob = $mob_mx
 	fill_x = 53
 	fill_height = 32
@@ -49,6 +69,10 @@ func death():
 	$visual/body/head.texture = face_defeated
 	for i in break_on_death:
 		i.texture = break_on_death[i]
+	if is_time_faster:
+		is_time_faster = false
+		Engine.time_scale = 1
+		time_fasting.get_node("anim").seek(8.8, true)
 	.death()
 
 
@@ -64,12 +88,12 @@ func get_phase():
 	if health_left <= 0:
 		return -1
 	elif health_left < 0.3:
-		next_attack_time_min = 0.8
-		next_attack_time_max = 1.6
+		next_attack_time_min = 0.7
+		next_attack_time_max = 1.4
 		return 3
 	elif health_left < 0.6:
-		next_attack_time_min = 0.9
-		next_attack_time_max = 1.8
+		next_attack_time_min = 0.85
+		next_attack_time_max = 1.7
 		return 2
 	next_attack_time_min = 1
 	next_attack_time_max = 2
@@ -80,10 +104,6 @@ func do_attack():
 	var phase = get_phase()
 	if phase < 0:
 		return
-	if global_position.distance_squared_to(player_target.global_position) < 6400:
-		if randi() % 2 == 0:
-			melee()
-			return
 	if phase > 2:
 		var new_variants = phases_attacks[phase].duplicate()
 		if not is_time_faster:
@@ -95,25 +115,93 @@ func do_attack():
 	call(phases_attacks[phase][0])
 
 
-func laser():
-	pass
-
-
-func super_floor():
-	pass
-
-
 func bullets():
-	pass
+	next_attack_time += 1.3
+	ms.sync_call(self, "bullets")
+	var direction = (global_position + Vector2.UP * 32).direction_to(player_target.global_position)
+	var angle = rad2deg(direction.angle_to(Vector2.DOWN))
+	animation.track_set_key_value(track_idx, key_idx0, angle)
+	animation.track_set_key_value(track_idx, key_idx1, angle)
+	anim.play("bullets")
+	yield(get_tree().create_timer(0.6, false), "timeout")
+	if not can_mob_move():
+		return
+	shoot.restart()
+	if not MP.auth(self):
+		return
+	for i in ball_spawns:
+		var gb = greenball.instance()
+		gb.global_position = i.global_position
+		gb.rotation = direction.angle()
+		get_tree().current_scene.add_child(gb)
+
+
+func laser(id = 0):
+	if MP.auth(self):
+		id = randi() % 2
+		ms.sync_call(self, "laser", [id])
+	anim.play("laser" + str(id))
+	next_attack_time += 2.6
+
+
+func super_floor(orange = false):
+	if MP.auth(self):
+		orange = randi() % 2 == 1
+		ms.sync_call(self, "super_floor", [orange])
+	next_attack_time += 4
+	anim.play("floor_attack")
+	yield(get_tree().create_timer(1.3, false), "timeout")
+	if not can_mob_move():
+		return
+	if orange:
+		floor_attack.play("attack_orange")
+	else:
+		floor_attack.play("attack_blue")
 
 
 func tnt():
-	pass
+	next_attack_time += 2.5
+	ms.sync_call(self, "tnt")
+	anim.play("tnt")
+	yield(get_tree().create_timer(0.7, false), "timeout")
+	if not can_mob_move() or not MP.auth(self):
+		return
+	shoot.restart()
+	var tnt = blue_tnt.instance()
+	tnt.global_position = shoot.global_position
+	tnt.velocity = Vector2(rand_range(-160, -32), rand_range(-250, -125))
+	get_tree().current_scene.add_child(tnt)
 
 
 func melee():
-	pass
+	next_attack_time += 1.6
+	ms.sync_call(self, "melee")
+	anim.play("kick")
+	yield(get_tree().create_timer(0.55, false), "timeout")
+	if not can_mob_move():
+		return
+	var to_pos = Vector2()
+	to_pos.x = min(player_target.global_position.x - global_position.x + 34, 0)
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property($visual, "position", to_pos, 0.25).set_ease(Tween.EASE_OUT)
+	tween.tween_interval(0.75)
+	tween.tween_property($visual, "position", Vector2.ZERO, 0.25).set_ease(Tween.EASE_IN_OUT)
 
 
 func time_fast():
-	pass
+	next_attack_time += 2
+	ms.sync_call(self, "time_fast")
+	anim.play("time_fast")
+	yield(get_tree().create_timer(0.6, false), "timeout")
+	if not can_mob_move():
+		return
+	var tf = time_fast.instance()
+	get_tree().current_scene.add_child(tf)
+	time_fasting = tf
+	yield(get_tree().create_timer(1.3, false), "timeout")
+	Engine.time_scale = 1.5
+	is_time_faster = true
+	yield(get_tree().create_timer(7.5, false), "timeout")
+	Engine.time_scale = 1
+	is_time_faster = false
